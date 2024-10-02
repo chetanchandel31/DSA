6a076f7a5d87052b74f29710d980546c178a46b9
chetan chandel <chetanchandel3@gmail.com>
2024-10-02 15:10:18 +0530

    improve min window substring

--- minimum-window-substring.ts
+++ minimum-window-substring.ts
@@ -1,58 +1,55 @@
 function minWindow(s: string, t: string): string {
-  // need
-  let needMap: Record<string, number> = {};
-  let need: number = 0;
+  // what we need
+  const needMap: Record<string, number> = {};
+  let need = 0;
+
   for (let i = 0; i < t.length; i++) {
     const char = t[i];
-    if (needMap[char]) {
-      needMap[char] += 1;
-    } else {
-      needMap[char] = 1;
+    if (!needMap[char]) {
       need++;
     }
+    needMap[char] = (needMap[char] || 0) + 1;
   }
 
-  // have
-  let haveMap: Record<string, number> = {};
+  // what we have
+  const haveMap: Record<string, number> = {};
   let have = 0;
-
-  // result
-  let minL = 0;
-  let minR = s.length; // so we can check if it was updated at all
 
   // sliding window
   let l = 0;
+  // keep track of min-sliding window
+  let minL = 0;
+  let minR = s.length; // to check if it was ever updated
 
   for (let r = 0; r < s.length; r++) {
     const char = s[r];
-    // increase HAVE
-    haveMap[char] = (haveMap[char] || 0) + 1;
-    if (haveMap[char] === needMap[char]) {
+    // update have
+    if (needMap[char]) {
+      haveMap[char] = (haveMap[char] || 0) + 1;
+    }
+    if (haveMap[char] && haveMap[char] === needMap[char]) {
       have++;
     }
 
-    // while need === have
     while (need === have) {
-      // check if found better result
-      if (r - l + 1 < minR - minL + 1) {
+      // update min-window
+      if (r - l < minR - minL) {
         minL = l;
         minR = r;
       }
-      // move l, update HAVE
-      if (s[l] in haveMap && haveMap[s[l]] === needMap[s[l]]) {
+      // update have
+      if (haveMap[s[l]] && haveMap[s[l]] === needMap[s[l]]) {
         have--;
       }
-      if (haveMap[s[l]] > 0) {
-        haveMap[s[l]]--;
+      if (haveMap[s[l]]) {
+        haveMap[s[l]] = haveMap[s[l]] - 1;
       }
-      if (haveMap[s[l]] === 0) {
-        delete haveMap[s[l]];
-      }
+      // move l
       l++;
     }
   }
 
-  return minR - minL + 1 > s.length ? "" : s.slice(minL, minR + 1);
+  return minR === s.length ? "" : s.slice(minL, minR + 1);
 }
 
 console.log(minWindow("t", "z"));
